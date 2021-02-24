cde87653ea47b94732352c61741f40618eca0088
Miguel <miguellukas52@gmail.com>
2021-02-23 21:16:52 -0300

    add files

---
+++ fuzzy_set.py
@@ -0,0 +1,47 @@
+from math import exp
+
+
+class FuzzySet:
+    TRIANGULAR = 0
+    TRAPEZOIDAL = 1
+    SINUSOIDAL = 2
+
+    def __init__(self, set_type, values):
+        self.type = set_type
+
+        self.values = values
+
+    def calculate_pertinence(self, value):
+        if self.type == 0:
+            return self._calculate_triangular_pertinence(value)
+        elif self.type == 1:
+            return self._calculate_trapezoidal_pertinence(value)
+        else:
+            return self._calculate_sinusoidal_pertinence(value)
+
+    def _calculate_triangular_pertinence(self, value):
+        if value <= self.values[0]:
+            return .0
+        elif self.values[0] < value <= self.values[1]:
+            return (value - self.values[0]) / (self.values[1] - self.values[0])
+        elif self.values[1] <= value <= self.values[2]:
+            return (value - self.values[2]) / (self.values[1] - self.values[2])
+        else:
+            return .0
+
+    def _calculate_trapezoidal_pertinence(self, value):
+        if self.values[0] <= value < self.values[1]:
+            return (value - self.values[0]) / (self.values[1] - self.values[0])
+        elif self.values[1] <= value <= self.values[2]:
+            return 1.0
+        elif self.values[2] < value <= self.values[3]:
+            return (self.values[3] - value) / (self.values[3] - self.values[2])
+        else:
+            return .0
+
+    def _calculate_sinusoidal_pertinence(self, value):
+        if (self.values[1] - self.values[2]) <= value <= (self.values[1] + self.values[2]):
+            return exp((-pow((value - self.values[1]), 2.0)) / self.values[0])
+        else:
+            return .0
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from matplotlib.ticker import ScalarFormatter
+
+from fuzzy_set import FuzzySet
+import matplotlib.pyplot as plt
+
+low_values = FuzzySet(FuzzySet.TRIANGULAR, [1, 15, 30])
+medium_values = FuzzySet(FuzzySet.TRIANGULAR, [15, 30, 45])
+high_values = FuzzySet(FuzzySet.TRIANGULAR, [30, 45, 60])
+
+sets = [low_values, medium_values, high_values]
+
+values = [[], [], []]
+pertinence_set = [[], [], []]
+
+print(" ")
+
+for i in range(len(sets)):
+    for value in range(61):
+        values[i].append(value)
+
+        pertinence_set[i].append(sets[i].calculate_pertinence(value))
+
+fig, axis = plt.subplots()
+
+
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+
+for i in range(len(values)):
+    axis.plot(values[i], pertinence_set[i])
+    for ax in [axis.xaxis, axis.yaxis]:
+        ax.set_major_formatter(formatter)
+
+plt.show()
